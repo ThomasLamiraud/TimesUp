00e2a518ada5c518441cf1e37a867930acbb2ab5
Thomas Lamiraud <thomas.lamiraud@viacesi.fr>
2020-04-25 23:18:26 +0200

    make the app fancy again

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,6 +2,6 @@ class WelcomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def welcome
-    @user_games = Game.where(user_id: current_user.id) if current_user
+    @user_games = Game.where(user_id: current_user.id).order(created_at: :desc) if current_user
   end
 end
